7ca8c14975f0245516904adbf397fe77faec450f
Uros Lates <uroslates@gmail.com>
2021-11-06 11:09:07 +0100

    initial commit

---
+++ apps/url-storefront/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'url-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'url-storefront';
+}
